da5405036e6939e19ce1e9139e5a1d17ac0d548e
ketakipillay <ketakipillay@gmail.com>
2022-01-30 13:05:26 -0500

    Constructors

--- src/constructors/Account.java
+++ src/constructors/Account.java
@@ -5,36 +5,85 @@ public class Account {
 	String accountName;
 	String accountNumber;
 	double balance;
-	
-	//No arg constructor
-	Account(){
-		
+
+	double getBalance() {
+		return balance;
 	}
 
+	void setBalance(double balance) {
+		this.balance = balance;
+	}
 
+	String cardNumber;
+
+	String getCardNumber() {
+		return cardNumber;
+	}
+
+	void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	double minBalance =50;
 	
+	int pinNumber;
 	
+	// No arg constructor
+	Account() {
+
+	}
+
+	// Parameterized constructor
 	Account(String accountName) {
 
 		this.accountName = accountName;
 	}
 
+	// There can be any number of constructors in a class
+	// given they have different input arguments
+	Account(String acctName, String acctNumber, double bal) {
 
-
-	Account(String acctName, String acctNumber, double bal){
-		
 		accountName = acctName;
 		accountNumber = acctNumber;
 		balance = bal;
 	}
+
+	//Create a parameterized constructor to initialize cardnum and pin
 	
-	Account(double bal){
+	Account(String cardNumber, int pinNumber) {
+		super();
+		this.cardNumber = cardNumber;
+		this.pinNumber = pinNumber;
+	}
+
+	Account(double bal) {
 		balance = bal;
 	}
-	
+
 	void setAccountName(String acctName) {
-		
+
 		String name;
 		accountName = acctName;
 	}
+	
+	boolean cardVerification(String cardNo, int pin) {
+		if (cardNo.equals(cardNumber) && (pin == pinNumber)){
+			return true;
+		}
+		return false;
+	}
+	double withDrawMoney(double amountToBeWithdrawn, String cardNo, int pin) {
+
+		// Call method to verify card
+			
+			// Compare cardNumber
+			if (cardVerification(cardNo, pin) && (balance - amountToBeWithdrawn) >= minBalance) {
+				
+				balance = balance - amountToBeWithdrawn;
+				return amountToBeWithdrawn;
+
+			}
+
+			return 0;
+		}
 }
--- src/constructors/AccountMain.java
+++ src/constructors/AccountMain.java
@@ -5,33 +5,34 @@ public class AccountMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		/*Constructor is used to create object of the class
-		 * . Java provides a default constructor if one is not declared
-		 * Constructor has same name as class
-		 * Constructor does not have a return type
-		 * it is quite similar to method
+		/*
+		 * Constructor is used to create object of the class . Java provides a default
+		 * constructor if one is not declared Constructor has same name as class
+		 * Constructor does not have a return type it is quite similar to method
 		 */
 		Account obj1 = new Account();
 		obj1.setAccountName("Priya");
-		
-		System.out.println("Account Name 1:"+obj1.accountName);
-		System.out.println("Account 1 balance : "+obj1.balance);
-		
-		
+
+		System.out.println("Account Name 1:" + obj1.accountName);
+		System.out.println("Account 1 balance : " + obj1.balance);
+
 		Account obj2 = new Account("Manvir");
-		
-		System.out.println("Account Name 2:"+obj2.accountName);
-		System.out.println("Account 2 balance : "+obj2.balance);
-		
-		Account obj3 = new Account("Thomas","786868686",10.0);
-		System.out.println("Account Name 3 :"+obj3.accountName);
-		System.out.println("Account 3 balance : "+obj3.balance);
-	
-		
+
+		System.out.println("Account Name 2:" + obj2.accountName);
+		System.out.println("Account 2 balance : " + obj2.balance);
+
+		Account obj3 = new Account("Thomas", "786868686", 10.0);
+		System.out.println("Account Name 3 :" + obj3.accountName);
+		System.out.println("Account 3 balance : " + obj3.balance);
+
 		Account obj4 = new Account(500);
-		System.out.println("Account Name 4 :"+obj4.accountName);
-		System.out.println("Account 4 balance : "+obj4.balance);
+		System.out.println("Account Name 4 :" + obj4.accountName);
+		System.out.println("Account 4 balance : " + obj4.balance);
 		
+		Account obj5 = new Account("4111111111111111", 1111);
+		obj5.setBalance(500);
+		obj5.withDrawMoney(100, obj5.getCardNumber(), obj5.pinNumber);
+
 	}
 
 }
